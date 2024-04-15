16e61cd5d1292ce0b1b09491761b9b6f3dd7e9bc
Yashhuc <yashsisodia155@gmail.com>
2024-04-15 21:34:02 +0530

    Initial commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,5 +127,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('DJANGO_MAIL')
-EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_MAIL_PASSWORD')
+EMAIL_HOST_USER = config('DJANGO_MAIL')
+EMAIL_HOST_PASSWORD = config('DJANGO_MAIL_PASSWORD')
